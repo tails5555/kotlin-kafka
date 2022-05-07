e7a7a713ffb3765e57376cf995ad85ca73231f51
tails5555 <tails5555@naver.com>
2022-05-07 15:54:23 +0900

    [Kang] Producer 모듈 제거

--- producer/src/main/kotlin/net/kang/producer/repository/ProducerDataRepository.kt
+++
@@ -1,9 +0,0 @@
-package net.kang.producer.repository
-
-import net.kang.producer.domain.ProducerData
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ProducerDataRepository: CrudRepository<ProducerData, Long> {
-}
--- producer/src/main/kotlin/net/kang/producer/service/ProducerService.kt
+++
@@ -1,7 +0,0 @@
-package net.kang.producer.service
-
-import net.kang.producer.domain.ProducerData
-
-interface ProducerService {
-    fun loadAll(): List<ProducerData>
-}
